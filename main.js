fbd1b2905c7af97fcbafc6884fa18642f5a6e07a
Tony Stark (lux copy) <158560749+abdullohabdurahmonov@users.noreply.github.com>
2024-03-11 23:06:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,280 @@
+// ALIF
+document.getElementById('alifButton').addEventListener('click', function() {
+    var audio = document.getElementById('alif');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Baa
+document.getElementById('baaButton').addEventListener('click', function() {
+    var audio = document.getElementById('baa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Taa
+document.getElementById('taaButton').addEventListener('click', function() {
+    var audio = document.getElementById('taa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Tha
+document.getElementById('thaaButton').addEventListener('click', function() {
+    var audio = document.getElementById('thaa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Jiym
+document.getElementById('jiymButton').addEventListener('click', function() {
+    var audio = document.getElementById('jiym');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Haa
+document.getElementById('haaButton').addEventListener('click', function() {
+    var audio = document.getElementById('haa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Xoo
+document.getElementById('xooButton').addEventListener('click', function() {
+    var audio = document.getElementById('xoo');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Dal
+document.getElementById('dalButton').addEventListener('click', function() {
+    var audio = document.getElementById('dal');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Dhal
+document.getElementById('dhalButton').addEventListener('click', function() {
+    var audio = document.getElementById('dhal');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Roo
+document.getElementById('rooButton').addEventListener('click', function() {
+    var audio = document.getElementById('roo');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//zaa
+document.getElementById('zaaButton').addEventListener('click', function() {
+    var audio = document.getElementById('zaa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Siiyn
+document.getElementById('siynButton').addEventListener('click', function() {
+    var audio = document.getElementById('siyn');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Shiiyn
+document.getElementById('shiiynButton').addEventListener('click', function() {
+    var audio = document.getElementById('shiiyn');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Sood
+document.getElementById('soodButton').addEventListener('click', function() {
+    var audio = document.getElementById('sood');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Doodd
+document.getElementById('dooddButton').addEventListener('click', function() {
+    var audio = document.getElementById('doodd');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Toa
+document.getElementById('toaButton').addEventListener('click', function() {
+    var audio = document.getElementById('toa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Dhaa
+document.getElementById('dhaaButton').addEventListener('click', function() {
+    var audio = document.getElementById('dhaa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Ain
+document.getElementById('ainButton').addEventListener('click', function() {
+    var audio = document.getElementById('ain');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Gain
+document.getElementById('gainButton').addEventListener('click', function() {
+    var audio = document.getElementById('gain');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Faa
+document.getElementById('faaButton').addEventListener('click', function() {
+    var audio = document.getElementById('faa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Qoof
+document.getElementById('qoofButton').addEventListener('click', function() {
+    var audio = document.getElementById('qoof');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Kaaf
+document.getElementById('kaafButton').addEventListener('click', function() {
+    var audio = document.getElementById('kaaf');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Laam
+document.getElementById('laamButton').addEventListener('click', function() {
+    var audio = document.getElementById('laam');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Miim
+document.getElementById('miimButton').addEventListener('click', function() {
+    var audio = document.getElementById('miim');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Nuuvn
+document.getElementById('nuuvnButton').addEventListener('click', function() {
+    var audio = document.getElementById('nuuvn');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Vaav
+document.getElementById('vaavButton').addEventListener('click', function() {
+    var audio = document.getElementById('vaav');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Ha
+document.getElementById('haButton').addEventListener('click', function() {
+    var audio = document.getElementById('ha');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
+//Yaa
+document.getElementById('yaaButton').addEventListener('click', function() {
+    var audio = document.getElementById('yaa');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
